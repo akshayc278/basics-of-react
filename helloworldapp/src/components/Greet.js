432750df980710f500efd102e7a3f7d0c83a17ad
akshayc278 <akshayc278@gmail.com>
2022-06-10 23:09:57 +0530

    functional component

---
+++ helloworldapp/src/components/Greet.js
@@ -0,0 +1,12 @@
+//Functional Component
+import React from 'react'
+
+function Greet() {
+    return <h1>Hello Akshay</h1>
+}
+
+export default Greet
+
+//ES 6 
+
+export const Greet1 = () => <h1>Hello Akshay ES6</h1>
